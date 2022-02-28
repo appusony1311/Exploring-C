7e54c2b259702976b990a8238ec13b0557c538f2
appusony1311 <97038752+appusony1311@users.noreply.github.com>
2022-02-28 20:56:24 +0530

    Update bits.c

--- Algorithms/Bits/bits.c
+++ Algorithms/Bits/bits.c
@@ -1,2 +1,15 @@
 
 //bits.c
+
+//Macro to set a bit
+#define bitset(byte,nbit)   ((byte) |=  (1<<(nbit)))
+
+//Toggle a bit
+#define bitflip(byte,nbit)  ((byte) ^=  (1<<(nbit)))
+
+//Clear a bit
+#define bitclear(byte,nbit) ((byte) &= ~(1<<(nbit)))
+#define bitflip(byte,nbit)  ((byte) ^=  (1<<(nbit)))
+
+//Checking if bit at nth position is set or not
+#define bitcheck(byte,nbit) ((byte) &   (1<<(nbit)))
